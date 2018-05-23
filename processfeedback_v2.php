643a534e8c2039c80fe2c7a19b1594ddfef13502
Dimitrios Tsarouchas <dimistsar@gmail.com>
2018-05-23 22:29:46 +0300

    Update processfeedback_v2.php

--- processfeedback_v2.php
+++ processfeedback_v2.php
@@ -26,7 +26,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Bob's Auto Parts - Feedback Submitted</title>
+    <title>Smart Form Mail - Comments</title>
 </head>
 <body>
 
